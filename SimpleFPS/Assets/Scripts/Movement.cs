40a2bdc3624a3d33142b6a8dbaaa97041c9f193d
Zjonn <Zjonnn@gmail.com>
2018-05-26 12:58:25 +0200

    I added Unity Basic Asset

---
+++ SimpleFPS/Assets/Scripts/Movement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		foreach(var touch in Input.touches)
+        {
+
+        }
+	}
+}
